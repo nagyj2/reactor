8bb144eba514b7b3a5be5ae5a6f193fa0cf753c0
Jason Nagy <jaysun_n@hotmail.com>
2025-01-19 09:34:14 -0500

    Add settings default toggle.

--- src/Settings.py
+++ src/Settings.py
@@ -11,12 +11,12 @@ class Settings(object):
             cls.instance = super(Settings, cls).__new__(cls)
         return cls.instance
 
-    def load_settings(self, config):
-        self.WIDTH = config.getint('screen', 'width')
-        self.HEIGHT = config.getint('screen', 'height')
-        self.FPS = config.getint('screen', 'fps')
+    def load_settings(self, config, default=False):
+        self.WIDTH = config.getint('screen', 'width') if not default else 640
+        self.HEIGHT = config.getint('screen', 'height') if not default else 480
+        self.FPS = config.getint('screen', 'fps') if not default else 30
 
-        self.PHYSICS_GRID = config.getboolean('physics', 'grid')
+        self.PHYSICS_GRID = config.getboolean('physics', 'grid') if not default else False
 
         self.TITLE = "RBMK Reactor Simulation"
         self.entity_id = 0  # provided to all Entities and incremented
@@ -25,7 +25,7 @@ class Settings(object):
 GlobalSettings = Settings()
 
 
-def init_settings():
+def init_settings(default=True):
     global GlobalSettings, config_filepath
 
     thisfolder = os.path.dirname(os.path.abspath(__file__))
